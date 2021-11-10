a87d0b9ec3528a2045f6f168c5354a5ecda90a81
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2021-11-10 16:06:27 +0600

    add

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Carousel, Container } from 'react-bootstrap';
+import './Banner.css'
+import image1 from '../../../images/banner/s-7.jpg'
+import image2 from '../../../images/banner/s-13.jpg'
+import image3 from '../../../images/banner/s-6.jpg'
 
 const Banner = () => {
     return (
-        <div>
-            <h2>This is banner</h2>
-        </div>
+        <Container className="mt-2 my-4">
+
+
+            <Carousel variant="white">
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 slide-img"
+                        src={image1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h5 className="text-color">One of the main benefits of wearing perfume is enhancing the mood</h5>
+                        
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 slide-img"
+                        src={image2}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <h5 className="text-color"> Perfume helps lift our spirits and reflects mood</h5>
+                        
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 slide-img"
+                        src={image3}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        <h5 className="text-color">perfumes offer many different kinds of smells for different moods</h5>
+                       
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </Container>
     );
 };
 
