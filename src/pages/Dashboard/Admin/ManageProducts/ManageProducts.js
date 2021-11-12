d137ea8433e332e5c0941dc92b587789d11ce90d
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2021-11-12 21:55:59 +0600

    add

--- src/pages/Dashboard/Admin/ManageProducts/ManageProducts.js
+++ src/pages/Dashboard/Admin/ManageProducts/ManageProducts.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import './ManageProducts.css'
 
 const ManageProducts = () => {
+    const [manageProducts, setManageProducts ] = useState([]);
+
+     useEffect(() => {
+         fetch('https://desolate-sea-37549.herokuapp.com/products')
+            .then(res => res.json())
+            .then(data => setManageProducts(data))
+     }, [])
+
+
+    const handleDelete = id => {
+        const url = `https://desolate-sea-37549.herokuapp.com/products/${id}`
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                //console.log(data);
+                if (data.deletedCount) {
+                    alert('Do you really want to delete it?')
+                    const remaining = manageProducts.filter(manageProduct => manageProduct._id !== id);
+                    setManageProducts(remaining);
+                }
+
+            })
+    }
+
+
+
+
     return (
         <div>
-            <h2>This is manage products</h2>
+            <h2 className="mb-5">Manage <span className="text-color later-color">Products</span> </h2>
+            
+     <Container>
+               <div className="manage-products">
+                    {
+                        manageProducts.map(manageProduct => <div
+                            key={manageProduct._id}>
+
+                            <div className="manage-product">
+                                <img className="w-25" src={manageProduct.img} alt="" />
+                                <h4 className="name-text my-2">{manageProduct.name}</h4>
+                                <small className="description-text">{manageProduct.description}</small>
+                                <h6 className="price-text"><p>Price: {manageProduct.price}</p></h6>
+                                <button className="btn btn-secondary" onClick={() => handleDelete(manageProduct._id)}>Remove</button>
+                            </div>
+
+                        </div>)
+                    }
+               </div>
+            </Container>
+
         </div>
     );
 };
