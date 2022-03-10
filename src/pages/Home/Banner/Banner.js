28810d826cf76a3745f43e0bd543c8333988a2d7
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-03-10 17:10:55 +0600

    add

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -28,45 +28,6 @@ const Banner = () => {
           </Card>
         </div>
       </div>
-
-      // <Container className="mt-2 my-4">
-
-      //     <Carousel variant="white">
-      //         <Carousel.Item>
-      //             <img
-      //                 className="d-block w-100 slide-img"
-      //                 src={image1}
-      //                 alt="First slide"
-      //             />
-      //             <Carousel.Caption>
-      //                 <h5 className="text-color">One of the main benefits of wearing perfume is enhancing the mood</h5>
-
-      //             </Carousel.Caption>
-      //         </Carousel.Item>
-      //         <Carousel.Item>
-      //             <img
-      //                 className="d-block w-100 slide-img"
-      //                 src={image2}
-      //                 alt="Second slide"
-      //             />
-      //             <Carousel.Caption>
-      //                 <h5 className="text-color"> Perfume helps lift our spirits and reflects mood</h5>
-
-      //             </Carousel.Caption>
-      //         </Carousel.Item>
-      //         <Carousel.Item>
-      //             <img
-      //                 className="d-block w-100 slide-img"
-      //                 src={image3}
-      //                 alt="Third slide"
-      //             />
-      //             <Carousel.Caption>
-      //                 <h5 className="text-color">perfumes offer many different kinds of smells for different moods</h5>
-
-      //             </Carousel.Caption>
-      //         </Carousel.Item>
-      //     </Carousel>
-      // </Container>
     );
 };
 
